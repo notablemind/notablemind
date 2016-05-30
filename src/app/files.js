3f1dad1ea7e28315a95cac660b863ab2c607bf9d
Jared Forsyth <jared@jaredforsyth.com>
2016-05-30 15:46:26 +0200

    giving up on syncing for the moment

--- src/app/files.js
+++ src/app/files.js
@@ -1,4 +1,4 @@
-var IxPL = require('treed/pl/firebase')
+var IxPL = require('treed/pl/ixdb')
   , QueuePL = require('treed/pl/queuedb')
   , treed = require('treed')
   , kernelConfig = require('./kernels')
