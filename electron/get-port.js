fde1c0a1b7d2a902a636e537c1d9e9db237ca01f
Jared Forsyth <jared@jaredforsyth.com>
2016-05-30 15:07:40 +0200

    fix port finding... also firebase isn't what I want

--- electron/get-port.js
+++ electron/get-port.js
@@ -1,18 +1,36 @@
 const net = require('net')
 var portrange = 45032
 
-module.exports = function getPort (cb) {
-  var port = portrange
-  portrange += 1
+var port = 45032
 
-  var server = net.createServer()
-  server.listen(port, function (err) {
-    server.once('close', function () {
-      cb(port)
-    })
-    server.close()
+var isPortTaken = function(port, fn) {
+  var net = require('net')
+  var tester = net.createServer()
+  .once('error', function (err) {
+    console.log('err', err)
+    if (err.code != 'EADDRINUSE') return fn(err)
+    fn(null, true)
   })
-  server.on('error', function (err) {
-    getPort(cb)
+  .once('listening', function() {
+    console.log('listen', port, tester)
+    tester.once('close', function() { fn(null, false) })
+    .close()
   })
+  .listen(port, 'localhost')
+}
+
+module.exports = function getPort (cb) {
+  const next = () => {
+    isPortTaken(port, (err, isTaken) => {
+      if (err) throw err
+      if (isTaken) {
+        port += 1;
+        next();
+      } else {
+        cb(port)
+      }
+    });
+  }
+
+  next()
 }
