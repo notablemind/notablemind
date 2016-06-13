b5bf40dc2d7869ecb25eaa89afb532da9079b42c
Jared Forsyth <jared@jaredforsyth.com>
2016-06-12 19:14:50 -0600

    some styling fixes

--- src/app/components/type-switcher.js
+++ src/app/components/type-switcher.js
@@ -1,5 +1,6 @@
 
 import React from 'react'
+import {StyleSheet, css} from 'aphrodite'
 
 export default React.createClass({
   propTypes: {
@@ -24,16 +25,21 @@ export default React.createClass({
   render: function () {
     var keys = Object.keys(this.props.types)
       , current = this.props.type
-    return <div className='TypeSwitcher'>
+    return <div className={css(styles.TypeSwitcher)}>
       <div
           onClick={this._toggleOpen}
-          className={'TypeSwitcher_icon TypeSwitcher_icon-' + current}>
+          className={css(
+            styles.icon,
+            styles['icon_' + current]
+          )}
+      >
         {current}
       </div>
       {this.state.open &&
-        <ul className='TypeSwitcher_dropdown'>
+        <ul className={css(styles.dropdown)}>
           {keys.map(key => key !== current &&
-            <li className={'TypeSwitcher_item TypeSwitcher_item-' + key}
+            <li className={css(styles.item,
+                               styles['item_' + key])}
                 onClick={this._onSelect.bind(null, key)}
                 key={key}>
               {key}
@@ -43,3 +49,34 @@ export default React.createClass({
   }
 })
 
+const styles = StyleSheet.create({
+  TypeSwitcher: {
+    position: 'relative',
+  },
+
+  icon: {
+    padding: '5px 10px',
+    cursor: 'pointer',
+  },
+
+  dropdown: {
+    position: 'absolute',
+    top: '100%',
+    right: 0,
+    zIndex: 100,
+    padding: 0,
+    margin: 0,
+    listStyle: 'none',
+    backgroundColor: 'white',
+    boxShadow: '0px 1px 1px #aaa',
+  },
+
+  item: {
+    cursor: 'pointer',
+    padding: '5px 10px',
+
+    ':hover': {
+      backgroundColor: '#eee',
+    },
+  },
+})
