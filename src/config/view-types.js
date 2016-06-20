131d8d68bd9a866512e6ad15d05ac5c3133dc27d
Jared Forsyth <jared@jaredforsyth.com>
2016-06-19 20:51:13 -0600

    add exhibit view type

--- src/config/view-types.js
+++ src/config/view-types.js
@@ -5,6 +5,7 @@ const viewTypes = {
   mindmap: require('treed/views/mindmap'),
   paper: require('treed/views/paper'),
   focus: require('treed/views/focus'),
+  exhibit: require('itreed/exhibit').default,
 }
 
 export default viewTypes
